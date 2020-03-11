b6f5314a864ac768a27627fba18ff40853810cd7
jpm <jpmckenna90@gmail.com>
2020-03-10 21:47:52 -0400

    Heroku Troubleshooting

--- models/index.js
+++ models/index.js
@@ -9,11 +9,13 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+var sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD);
 
 fs
   .readdirSync(__dirname)
