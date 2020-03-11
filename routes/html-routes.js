2be7f48ae9e779132f480e6f340d12fb5f8c1b87
jpm <jpmckenna90@gmail.com>
2020-03-10 20:57:10 -0400

    Fixed route after submitting trip criteria

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,4 +27,12 @@ module.exports = function (app) {
   app.get("/main", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
+
+  app.get("/itinerary", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/choices.html"))
+  })
+
+  app.get("/main", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/main.html"));
+  })
 };
