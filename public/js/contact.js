27acb924d9b93502bfeeaf8f552c0adf34d2a8e4
tdj03001 <tyjenki@gmail.com>
2020-03-12 21:10:13 -0400

    Contact and about (#37)
    * new potential background imagery
    
    * added a few classes and id's for styling purposes
    
    * added mailto: functionality so clients can contact us via email
    
    * new css file for each of the main.html and the contact.html pages
    
    * new html page for contacting us

---
+++ public/js/contact.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  $("#submitbutton").on("click", function () {
+    event.preventDefault();
+    var senderName = $("#namefield").val();
+    var emailFrom = $("#emailfield").val();
+    var emailBody = $("#messagefield").val();
+    emailBody = emailBody + "%0A" + "%0A" + "Sender's Name: " + senderName + " | " + "  Sender's Email: " + emailFrom + "%0A";
+    $("emailBody").append(emailFrom);
+    window.location.href = "mailto:JNTenterprises2@gmail.com?&subject=Travel%20inquiry&body=" + emailBody;
+
+
+  });
+
+
+});
