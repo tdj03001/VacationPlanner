b4025430c2a22ee2e1805b01191bcf6d6ba5e793
tdj03001 <tyjenki@gmail.com>
2020-03-13 20:40:04 -0400

    removed variables at top of original doc since jQuery can just call what we need (email-input, password-input, name-input) by id. Where those variables were previously used I changed them to have jQuery call them by id

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
-  // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
-  var nameInput = $("input#name-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function (event) {
+  $("#signupBtn").on("click", function (event) {
     event.preventDefault();
     var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim(),
-      name: nameInput.val().trim()
+      email: $("#email-input").val().trim(),
+      password: $("#password-input").val().trim(),
+      name: $("#name-input").val().trim()
     };
 
     if (!userData.email || !userData.password) {
@@ -19,9 +14,9 @@ $(document).ready(function () {
     }
     // If we have an email and password, run the signUpUser function
     signUpUser(userData.email, userData.password, userData.name);
-    emailInput.val("");
-    passwordInput.val("");
-    nameInput.val("");
+    $("#email-input").val("");
+    $("#password-input").val("");
+    $("#name-input").val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
