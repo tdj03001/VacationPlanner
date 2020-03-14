eb89bec41af562b4949b8047b6d7961bfa8137fc
tdj03001 <tyjenki@gmail.com>
2020-03-13 22:26:05 -0400

    Contact and about (#53)
    * new potential background imagery
    
    * added a few classes and id's for styling purposes
    
    * added mailto: functionality so clients can contact us via email
    
    * new css file for each of the main.html and the contact.html pages
    
    * new html page for contacting us
    
    * updated email address
    
    * new stylesheets for index.html and for signup.html
    
    * refactored to remove bootstrap and use Bulma instead
    
    * updated favicon
    
    * reverting back to bootstrap for now. There was too much dependent code that would have to be changed to support switching from bootstrap to bulma at this late stage in the project.
    
    * updated styling for contact page
    
    * I put the original code back in. If we have time we'll bulma-ize index.html and signup.html
    
    * linked the new about page
    
    * style updates for contact page (darker send button) and created new stylesheet for About page
    
    * initial commit
    
    * new background image for About page
    
    * fixed navbar img sizing
    
    * navbar now collapses on small screen and menu items open on tap or click from a hamburger
    
    * initial commit. This is the logic for the collapsing and expanding navbar
    
    * removing '<<<<<<<<<<< HEAD>>>>>>>>'
    
    * removed the commented-out lines referring to choices.js
    
    * added styling to the template literal for coloring
    
    * added styling and alignment for cards
    
    * minor styling updates
    
    * added 'vacation planner' to navbar so the login and signup pages at least have a reference to where they go!
    
    * removed bootstrap, added bulma, gave email, password, and name inputs an id to be called by login.js and index.js
    
    * removed variables at top of original doc since jQuery can just call what we need (email-input, password-input, name-input) by id. Where those variables were previously used I changed them to have jQuery call them by id
    
    * linked new background img
    
    * new background image for main page

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-  // Getting references to our form and inputs
-  var loginForm = $("form.login");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function (event) {
+  $("#loginBtn").on("click", function (event) {
     event.preventDefault();
     var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      email: $("#email-input").val().trim(),
+      password: $("#password-input").val().trim()
     };
 
     if (!userData.email || !userData.password) {
@@ -18,8 +14,8 @@ $(document).ready(function () {
 
     // If we have an email and password we run the loginUser function and clear the form
     loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    $("#email-input").val("");
+    $("#password-input").val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
